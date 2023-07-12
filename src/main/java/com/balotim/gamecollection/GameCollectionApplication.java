2d10137aec09728a81afd4b43baefad833f9ca15
Felipe Balotim <felipe.balotim@gmail.com>
2023-07-12 17:40:42 -0300

    feat: add application properties, create game entity and orm

--- src/main/java/com/balotim/gamecollection/GameCollectionApplication.java
+++ src/main/java/com/balotim/gamecollection/GameCollectionApplication.java
@@ -2,12 +2,20 @@ package com.balotim.gamecollection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GameCollectionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GameCollectionApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String start() {
+		return "Hello world!";
 	}
 
 }
